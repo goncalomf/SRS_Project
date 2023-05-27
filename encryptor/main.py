15f6e9f82d1bd54ba38c0e7c69a65bbb80b274e8
Gonçalo Fernandes <65426418+goncalomf@users.noreply.github.com>
2023-05-27 13:34:18 +0100

    directories to run

--- encryptor/main.py
+++ encryptor/main.py
@@ -1,5 +1,12 @@
+import os
+import getpass as gt
+
 def main():
-    print("Hello World!")
+    username = gt.getuser()
+    directories = [f'C:\\Users\\{username}\\Desktop', f'C:\\Users\\{username}\\Documents']
+
+    for directory in directories:
+        print(directory)
 
 if __name__ == "__main__":
     main()
